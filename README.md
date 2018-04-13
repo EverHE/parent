f0fbc6d8a7601922575ac720b512edb65678b35e
EverHE <heleiever@hotmail.com>
2018-04-13 17:22:32 +0800

    删除垃圾文件

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # parent
 初始提交
+
+lombok使用
+注：idea需要安装lombok插件并设置才能识别实体get set
+参考：https://blog.csdn.net/zhglance/article/details/54931430
--- web/src/main/java/com/he/web/controller/IndexController.java
+++ web/src/main/java/com/he/web/controller/IndexController.java
@@ -4,17 +4,26 @@ import com.alibaba.fastjson.JSONObject;
 import com.he.model.entity.User;
 import com.he.service.sys.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-@RestController
+@Controller
 public class IndexController {
     @Resource
-    IUserService    userService;
+    private IUserService    userService;
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
 
     @RequestMapping("/hello")
+    @ResponseBody
     public String hello(){
         JSONObject j = new JSONObject();
         j.put("key","value");
@@ -22,6 +31,7 @@ public class IndexController {
     }
 
     @RequestMapping("/add")
+    @ResponseBody
     public String test(){
         User u = new User();
         u.setUsername("hehe");
